d103a77f09656530b269dfcce0b8a537464a7f19
OnaniMasterG <gazachaoui@gmail.com>
2020-12-14 17:32:32 +0100

    td6 fonctionnel

--- app/src/main/java/com/example/projetandroid/userinfo/UserInfoActivity.kt
+++ app/src/main/java/com/example/projetandroid/userinfo/UserInfoActivity.kt
@@ -1,35 +1,29 @@
 package com.example.projetandroid.userinfo
 
 import android.Manifest
-import android.content.Context
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.util.AttributeSet
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.result.launch
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.core.net.toFile
-import androidx.core.net.toUri
+import androidx.core.content.FileProvider
 import androidx.lifecycle.lifecycleScope
 import coil.load
 import coil.transform.CircleCropTransformation
+import com.example.projetandroid.BuildConfig
 import com.example.projetandroid.R
-import com.example.projetandroid.network.Api
 import com.example.projetandroid.network.UserInfo
 import kotlinx.coroutines.launch
 import okhttp3.MultipartBody
-import okhttp3.RequestBody.Companion.asRequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
 import java.io.File
 
@@ -37,26 +31,16 @@ class UserInfoActivity : AppCompatActivity() {
 
     // register
     private val pickInGallery =
-        registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
-            handleImage1(uri)
-        }
+            registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+                handleImage(uri)
+            }
     private val userViewModel : UserInfoViewModel by viewModels()
 
-
-    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
-        return super.onCreateView(name, context, attrs)
-        lifecycleScope.launch {
-            userViewModel.userInfo = Api.userService.getInfo().body()
-        }
-    }
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_info)
-
-        var takePictureButton = findViewById<Button>(R.id.take_picture_button)
-        var uploadImageButton = findViewById<Button>(R.id.upload_image_button)
-        var sendImageButton = findViewById<Button>(R.id.send_user_info)
+/* var sendImageButton = findViewById<Button>(R.id.send_user_info)
 
         var firstName = findViewById<EditText>(R.id.edit_first_name)
         var lastName = findViewById<EditText>(R.id.edit_last_name)
@@ -65,10 +49,9 @@ class UserInfoActivity : AppCompatActivity() {
 
         takePictureButton.setOnClickListener {
             askCameraPermissionAndOpenCamera()
-        }
-        uploadImageButton.setOnClickListener {
+@ -55,6 +76,13 @@ class UserInfoActivity : AppCompatActivity() {
             // use
-            pickInGallery.launch("image/*")
+            pickInGallery.launch("image")
         }
 
         sendImageButton.setOnClickListener {
@@ -76,41 +59,55 @@ class UserInfoActivity : AppCompatActivity() {
             lifecycleScope.launch {
                 userViewModel.updateInfo(newUserInfo)
             }
+        }*/
+        val takePictureButton = findViewById<Button>(R.id.take_picture_button)
+        val uploadImageButton = findViewById<Button>(R.id.upload_image_button)
+        val sendImageButton = findViewById<Button>(R.id.send_user_info)
+        val firstName = findViewById<EditText>(R.id.edit_first_name)
+        val lastName = findViewById<EditText>(R.id.edit_last_name)
+        val email = findViewById<EditText>(R.id.edit_email)
+        val image = findViewById<ImageView>(R.id.test_image)
+
+        takePictureButton.setOnClickListener {
+            askCameraPermissionAndOpenCamera()
+        }
+        uploadImageButton.setOnClickListener {
+            pickInGallery.launch("image/*")
+        }
+
+        sendImageButton.setOnClickListener {
+            val newUserInfo = UserInfo(email.text.toString(), firstName.text.toString(), lastName.text.toString(), userViewModel.userInfo.value?.avatar.toString())
+            lifecycleScope.launch {
+                userViewModel.updateInfo(newUserInfo)
+            }
+        }
+        userViewModel.userInfo.observe(this) {
+            firstName?.setText(it.firstName)
+            lastName?.setText(it.lastName)
+            email?.setText(it.email)
+            image?.load(it.avatar) {
+                crossfade(true)
+                crossfade(1000)
+                transformations(CircleCropTransformation())
+            }
         }
     }
 
     override fun onResume() {
         super.onResume()
         lifecycleScope.launch {
-            var firstName = findViewById<EditText>(R.id.edit_first_name)
-            var lastName = findViewById<EditText>(R.id.edit_last_name)
-            var email = findViewById<EditText>(R.id.edit_email)
-            var image = findViewById<ImageView>(R.id.test_image)
-            userViewModel.userInfo = Api.userService.getInfo().body()
-            val userInfo = userViewModel.userInfo
-            //val userInfo = UserInfo("", "", "")
-            firstName?.setText(userInfo?.firstName)
-            lastName?.setText(userInfo?.lastName)
-            email?.setText(userInfo?.email)
-            image?.load(userInfo?.avatar) {
-                crossfade(true)
-                crossfade(1000)
-                transformations(CircleCropTransformation())
-            }
-        }
-        lifecycleScope.launch {
             userViewModel.refresh()
         }
     }
 
     private val requestPermissionLauncher =
-        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
-            if (isGranted) openCamera()
-            else showExplanationDialog()
-        }
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+                if (isGranted) openCamera()
+                else showExplanationDialog()
+            }
 
     private fun requestCameraPermission() =
-        requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+            requestPermissionLauncher.launch(Manifest.permission.CAMERA)
 
     @RequiresApi(Build.VERSION_CODES.M)
     private fun askCameraPermissionAndOpenCamera() {
@@ -132,7 +129,7 @@ class UserInfoActivity : AppCompatActivity() {
             show()
         }
     }
-/*
+
     private val photoUri by lazy {
         FileProvider.getUriForFile(
                 this,
@@ -145,36 +142,14 @@ class UserInfoActivity : AppCompatActivity() {
     // register
     private val takePicture = registerForActivityResult(ActivityResultContracts.TakePicture()) { success ->
         if (success) handleImage(photoUri)
-        else Toast.makeText(
-                this,
-                "Erreur ! 😢",
-                Toast.LENGTH_LONG
-        ).show()
+        else Toast.makeText(this, "Erreur ! 😢", Toast.LENGTH_LONG).show()
     }
 
     // use
     private fun openCamera() = takePicture.launch(photoUri)
-*/
-    // register
-    private val takePicture = registerForActivityResult(ActivityResultContracts.TakePicturePreview()) { bitmap ->
-        val tmpFile = File.createTempFile("avatar", "jpeg")
-        tmpFile.outputStream().use {
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, it)
-        }
-        handleImage(tmpFile.toUri())
-    }
 
-    // use
-    private fun openCamera() = takePicture.launch()
-
-    private fun convert(uri: Uri) =
-            MultipartBody.Part.createFormData(
-                    name = "avatar",
-                    filename = "temp.jpeg",
-                    body = uri.toFile().asRequestBody()
-            )
     // convert
-    private fun convert1(uri: Uri) =
+    private fun convert(uri: Uri) =
             MultipartBody.Part.createFormData(
                     name = "avatar",
                     filename = "temp.jpeg",
@@ -184,11 +159,7 @@ class UserInfoActivity : AppCompatActivity() {
     private fun handleImage(uri : Uri) {
         lifecycleScope.launch {
             userViewModel.updateAvatar(convert(uri))
-        }
-    }
-    private fun handleImage1(uri : Uri) {
-        lifecycleScope.launch {
-            userViewModel.updateAvatar(convert1(uri))
+            userViewModel.refresh()
         }
     }
 }
--- app/src/main/java/com/example/projetandroid/userinfo/UserInfoViewModel.kt
+++ app/src/main/java/com/example/projetandroid/userinfo/UserInfoViewModel.kt
@@ -1,21 +1,27 @@
 package com.example.projetandroid.userinfo
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.projetandroid.network.UserInfo
+import kotlinx.coroutines.launch
 import okhttp3.MultipartBody
 
 class UserInfoViewModel : ViewModel() {
     private val repository = UserInfoRepository()
-    public var userInfo : UserInfo? = null
+    private var _userInfo = MutableLiveData<UserInfo>()
+    var userInfo : LiveData<UserInfo> = _userInfo
 
-    suspend fun refresh() {//repository.refresh()
-        val infoResponse = repository.userService.getInfo()
-        // À la ligne suivante, on a reçu la réponse de l'API:
-        if (infoResponse.isSuccessful) {
-            val fetchedInfo = infoResponse.body()
-            userInfo = fetchedInfo!!
+    fun refresh() {//repository.refresh()
+        viewModelScope.launch {
+            val infoResponse = repository.userService.getInfo()
+            // À la ligne suivante, on a reçu la réponse de l'API:
+            if (infoResponse.isSuccessful) {
+                val fetchedInfo = infoResponse.body()
+                _userInfo.value = fetchedInfo!!
+            }
         }
-        else null
     }
 
     suspend fun updateInfo(userInfo : UserInfo) {repository.updateInfo(userInfo)}
